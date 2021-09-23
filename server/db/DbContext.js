8ed0aa6f244a266c5a61374eed25969dc1d89232
nathan <nateohendricks@gmail.com>
2021-09-22 19:48:46 -0600

    cars

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -3,10 +3,12 @@ import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
 import { HouseSchema } from '../models/House'
 import { JobSchema } from '../models/Job'
+import { CarSchema } from '../models/Car'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Cars = mongoose.model('Car', CarSchema)
   Houses = mongoose.model('House', HouseSchema)
   Jobs = mongoose.model('Job', JobSchema)
 }
